0a49e76e123cf02eecd7575884bd2eff68fd6ce8
7kia <il7kia@yandex.ru>
2016-10-26 14:31:31 +0300

    Add function IsBetween

--- PP2/stdafx.h
+++ PP2/stdafx.h
@@ -6,3 +6,9 @@
 #include <tchar.h>
 
 #include <memory>
+
+template <typename T>
+bool IsBetween(const T& value, const T& lowerBound, const T& upperBound)
+{
+	return (value >= lowerBound) && (value <= upperBound);
+}
