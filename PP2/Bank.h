2d27e0763954dc802b9f68830d99f45746fd7619
ASmMegane <a_n_d_i@bk.ru>
2016-12-29 16:47:49 +0300

    lab2 Sm.A.

--- PP2/Bank.h
+++ PP2/Bank.h
@@ -1,21 +1,26 @@
 #pragma once
 #include <iostream>
 #include <vector>
-
 #include "BankClient.h"
 
 class CBank
 {
 public:
-	CBank();
+	CBank(PRIMITIVE_TYPE type);
+	~CBank();
+
 	CBankClient* CreateClient();
 	void UpdateClientBalance(CBankClient& client, int value);
+
+	HANDLE* GetClientsHandles() const;
+	size_t GetClientsCount() const;
 
 private:
 	std::vector<CBankClient> m_clients;
 	int m_totalBalance;
+	std::shared_ptr<Primitives> m_syncPrimitives;
 
 	int GetTotalBalance();
 	void SetTotalBalance(int value);
-	void SomeLongOperations();
+	void SomeLongOperations(CBankClient const &client);
 };
